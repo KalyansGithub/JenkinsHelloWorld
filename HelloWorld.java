27ef2f26fc15a644e31db16813d5e5e88e215698
KalyansGithub <43195816+KalyansGithub@users.noreply.github.com>
2020-10-27 12:27:07 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld
 	public static void main(String[] args) 
 	{
 	 System.out.println("Hello, Welcome to Jenkins!");
-	 System.out.println("checking webhook trigger now");
+	 System.out.println("checking Poll scm now");
 	}
 }
