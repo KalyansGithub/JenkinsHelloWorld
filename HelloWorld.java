cc1b03477ed1b4a21becab7b371d4046aff87d49
KalyansGithub <43195816+KalyansGithub@users.noreply.github.com>
2020-10-28 10:15:19 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld
 	public static void main(String[] args) 
 	{
 	 System.out.println("Hello, Welcome to Jenkins!");
-	 System.out.println("checking any changes by Polling Github");
+	 System.out.println("checking any changes by Polling SCM");
 	}
 }
